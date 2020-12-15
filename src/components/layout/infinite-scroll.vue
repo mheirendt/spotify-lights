fa7624d291956e7fa2b93e709aacb4a5dccf8cd1
Michael Heirendt <michael.heirendt@ndpcci.com>
2020-12-15 11:02:28 -0800

    - no listeners for slot

--- src/components/layout/infinite-scroll.vue
+++ src/components/layout/infinite-scroll.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="infinite-scroll">
-    <slot :items="items" v-on="$listeners" />
+    <slot :items="items" />
     <div class="d-flex loading" v-if="items.length < total">
       <div class="mx-auto pt-2">
         <v-icon class="icon" color="primary" v-intersect="loadMore">{{
